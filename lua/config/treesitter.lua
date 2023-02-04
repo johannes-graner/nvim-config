9e6f8f685ca605166a3869f00d3a4ecc56b25d06
Johannes Graner <johannes.graner@gmail.com>
2023-02-04 16:39:16 +0100

    Treesitter indenting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "python", "cpp", "lua", "vim", "rust", "toml" },
   ignore_install = {}, -- List of parsers to ignore installing
+  indent = { enable = true },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { 'help' }, -- list of language that will be disabled
